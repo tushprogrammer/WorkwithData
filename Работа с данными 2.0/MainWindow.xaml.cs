58f34b5c33af9649666b5d782801b443e06578d0
Dmitriy Pepelyaev <pepelyaev-94@mail.ru>
2023-04-11 21:33:47 +0500

    в окне входа удаление строки подключения к БД добавление схемы на БД - workdataDB

--- Работа с данными 2.0/MainWindow.xaml.cs
+++ Работа с данными 2.0/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.OleDb;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,50 +23,28 @@ namespace Работа_с_данными_2._0
     /// </summary>
     public partial class MainWindow : Window
     {
-        string name_user = string.Empty; //имя админа (не понадобилось)
+        //string name_user = string.Empty; //имя админа (не понадобилось)
+        WorkDataDBEntities context;
         public MainWindow()
         {
-            InitializeComponent(); 
-            
-            
-            
-
-
+            InitializeComponent();
+            context = new WorkDataDBEntities();
         }
 
         private void Log_enter_Click(object sender, RoutedEventArgs e)
         {
-            SqlConnectionStringBuilder strCon = new SqlConnectionStringBuilder() //строка подлкючения к базе sql
-            {
-                DataSource = @"(localdb)\MSSQLLocalDB",
-                InitialCatalog = "WorkDataDB",
-                IntegratedSecurity = true,
-                Pooling = false
-            };
-            SqlConnection connection = new SqlConnection(strCon.ConnectionString); //создание подключения на основе строки
-            MessageBox.Show($"Строка подключения: {strCon.ConnectionString}"); //вывод строки подключения
+            
+
 
             try
             {
-                connection.Open();
-                SqlCommand com = new SqlCommand($"SELECT * FROM users WHERE ((([user_nickname]) ='{login_box.Text}') " +
-                    $"AND(([user_password]) = '{password_box.Password}'))", connection); //выборка логина и пароля
-                SqlDataReader reader = com.ExecuteReader();
+                var user = context.UsersSet.Where(w => w.user_nickname == login_box.Text && w.user_password == login_box.Text);
+                //SqlCommand com = new SqlCommand($"SELECT * FROM users WHERE ((([user_nickname]) ='{login_box.Text}') " +
+                //    $"AND(([user_password]) = '{password_box.Password}'))", connection); //выборка логина и пароля              
                 
-                if (!reader.Read()) //если по запросу вообще ничего не нашел
-                {
-                    MessageBox.Show("Проверьте правильность написания логина и пароля"); //
-                    return;
-                }
-                reader.Close();
+                
 
-                //reader = com.ExecuteReader(); //обновление запроса, так как после первой проверки он уже типо "прочитан"
-                //while (reader.Read())//если понадобится сохранить логин и пароль, что по сути бессмыслено
-                //                     //так как если прошлая проверка пройдена, значит введеный пользователь имеет доступ на вход
-                //                     //уже сам разочаровался в этом коде, но пусть будет
-                //{
-                //    name_user = Convert.ToString(reader[3]);                    
-                //}
+              
 
             }
             catch (Exception a)
@@ -75,7 +54,7 @@ namespace Работа_с_данными_2._0
             }
             finally
             {
-                connection.Close();//закрыть содединение и высвободить ресурсы
+                
                 //создание и открытие нового окна
                 DataInfo info = new DataInfo();
                 info.Show();
